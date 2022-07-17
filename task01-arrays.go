34ff4cb2936ac034b5df3bba0960f827a42567a3
Tazo <21140327+Tazo2016@users.noreply.github.com>
2022-07-17 22:38:37 +0300

    result1

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,11 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
+	var sum float32
+	//var average float32
+	for _, elem := range input {
+		sum += elem
+	}
+	result = sum / float32(len(input))
 	return
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,19 @@
 package homework
 
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
+	map_keys := make([]int, 0, len(input))	
+	for key := range input {
+    	map_keys = append(map_keys, key)		
+	}
+	for i := 0; i < len(map_keys); i++ {
+		for k := len(map_keys) - 1; k > i; k-- {
+			if map_keys[k-1] > map_keys[k] {
+				map_keys[k-1], map_keys[k] = map_keys[k], map_keys[k-1]
+			}
+		}
+	}
+	for _, k := range map_keys {
+		result = append(result, input[k])
+	}
 	return
 }
